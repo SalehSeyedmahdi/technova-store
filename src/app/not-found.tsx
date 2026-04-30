c0143fcf0e32f0a6148002eb22587e6b84f767a6
Saleh <hmohamad.sms@gmail.com>
2026-04-30 23:07:27 +0330

    feat: fix something

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ export default function NotFound() {
 	return (
 		<div className="w-full h-screen flex justify-center items-center text-[20px] bg-[#c2deef]">
 			<div className="flex justify-center items-center relative bg-[#ffffff] rounded-xl p-10 pr-15 pl-15">
-				<div className="w-full h-2/30 absolute top-0 bg-blue-700 rounded-t-xl"></div>
+				<div className="w-full h-3/30 absolute top-0 bg-blue-700 rounded-t-xl"></div>
 				<div className="flex flex-col justify-center items-center">
 					<h1 className="font-bold text-[120px]">404</h1>
 					<h2 className="text-[24px]">Page Not Found</h2>
