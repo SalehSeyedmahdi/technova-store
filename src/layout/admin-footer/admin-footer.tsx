3361177c0063049f64391137f90820e9d7d84626
Saleh <hmohamad.sms@gmail.com>
2026-04-30 21:47:47 +0330

    feat: update layouts

--- src/layout/admin-footer/admin-footer.tsx
+++ src/layout/admin-footer/admin-footer.tsx
@@ -1,16 +1,10 @@
 export default function AdminFooter() {
 	return (
-		<footer className="w-full fixed bottom-0 flex justify-center items-center font-[yekanBakh] text-[16px] bg-gray-600 p-[12px] pl-[14px] pr-[14px]">
+		<footer className="w-full fixed bottom-0 flex justify-center items-center font-[yekanBakh] text-[16px] bg-[#f0f0f0] inset-shadow-sm p-[14px] pl-[14px] pr-[14px]">
 			<ol className="flex gap-5">
-				<li className="cursor-pointer hover:text-blue-700 hover:border-b-2 hover:rounded-sm">
-					سفارش ها
-				</li>
-				<li className="cursor-pointer hover:text-blue-700 hover:border-b-2 hover:rounded-sm">
-					موجودی
-				</li>
-				<li className="cursor-pointer hover:text-blue-700 hover:border-b-2 hover:rounded-sm">
-					محصولات
-				</li>
+				<li className="cursor-pointer hover:text-[#0000ff]">سفارش ها</li>
+				<li className="cursor-pointer hover:text-[#0000ff]">موجودی</li>
+				<li className="cursor-pointer hover:text-[#0000ff]">محصولات</li>
 			</ol>
 		</footer>
 	);
