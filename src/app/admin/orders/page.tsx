74a2d91707d1243ff72091a375c3f97fd2fe4ebf
Saleh <hmohamad.sms@gmail.com>
2026-05-08 08:59:41 +0330

    feat: create add form in admin panel

--- src/app/admin/orders/page.tsx
+++ src/app/admin/orders/page.tsx
@@ -1,7 +1,9 @@
+import OrdersTable from "@/components/admin/components/orders-tabel";
+
 export default function OrdersPage() {
 	return (
-		<div className="w-full h-screen flex justify-center items-center bg-green-500">
-			OrdersPage
+		<div className="w-full h-screen flex justify-center items-center font-[YekanBakh] bg-[#eaebfc]">
+			<OrdersTable />
 		</div>
 	);
 }
---
+++ src/app/admin/products/add-product/page.tsx
@@ -0,0 +1,9 @@
+import AddForm from "@/components/admin/components/add-form";
+
+export default function AddProductPage() {
+	return (
+		<div className="w-full h-screen flex justify-center items-center font-[YekanBakh] bg-[#eaebfc]">
+			<AddForm />
+		</div>
+	);
+}
