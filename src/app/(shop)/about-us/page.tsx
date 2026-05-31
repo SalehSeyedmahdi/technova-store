5acbdde38ca036ff364c5e96a3253223fe89b597
Saleh <hmohamad.sms@gmail.com>
2026-05-31 17:33:18 +0330

    feat: create about us page

--- src/app/(shop)/about-us/page.tsx
+++ src/app/(shop)/about-us/page.tsx
@@ -1,7 +1,9 @@
+import AboutUs from "@/components/shop/components/about-us";
+
 export default function AboutUsPage() {
 	return (
-		<div className="w-full h-screen flex justify-center items-center bg-[#eaebfc]">
-			AboutUsPage
+		<div className="w-full h-full flex justify-center items-center font-[YekanBakh] bg-[#eaebfc] pt-[85px] md:pt-[140px] py-[20px] px-[20px] md:px-[40px]">
+			<AboutUs />
 		</div>
 	);
 }
